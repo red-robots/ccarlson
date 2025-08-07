b2788d9fe27f42b601ba113955acda88827f69f8
Lisa <lisaqdebona@gmail.com>
2025-08-07 04:26:42 -0400

    Styling portfolio page

--- page.php
+++ page.php
@@ -15,15 +15,16 @@ get_header(); ?>
 
 <div id="primary" class="content-area-full generic-layout">
 	<main id="main" class="site-main" role="main">
+  <?php if( have_rows('flexible_content') ) { ?>
+    <?php include( locate_template('parts/content-flexible.php') ); ?>
+  <?php } else { ?>
     <?php while ( have_posts() ) : the_post(); ?>
-      <div class="titlediv">
-        <div class="wrapper"><h1 class="page-title"><?php the_title(); ?></h1></div>
-      </div>
-      <div class="entry-content contentDiv">
-        <div class="wrapper"><?php the_content(); ?></div>
+      <div class="entry-content">
+        <?php the_content() ?>
       </div>
     <?php endwhile; ?>
-	</main><!-- #main -->
+  <?php } ?>
+</main>
 </div><!-- #primary -->
 
 <?php
